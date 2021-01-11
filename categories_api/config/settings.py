9457d41d1b70d0e2e3b2fca0d0e7de213a0d4271
Nikita Koznev <nikitakoznev@gmail.com>
2021-01-11 17:25:06 +0300

    Added docker files

--- categories_api/config/settings.py
+++ categories_api/config/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = os.environ.get('DATABASE_ENGINE')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(' ')
 
 
 # Application definition
